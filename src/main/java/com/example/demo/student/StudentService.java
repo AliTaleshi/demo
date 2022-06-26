82b13be47f3563c0ceb15df93d3424f0f0512c9f
AliTaleshi <alitys083@gmail.com>
2022-06-26 19:07:10 +0430

    V2

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,11 @@
+package com.example.demo.student;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+    public void sayHelloWorld() {
+        System.out.println("Hello World!");
+    }
+}
